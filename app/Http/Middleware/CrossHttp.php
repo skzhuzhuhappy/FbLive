21d4a1c3041ad14f97d81a8fd0bc87d658480020
skzhuzhu <15201255789@163.com>
2020-05-24 17:27:57 +0800

    跨域

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -17,7 +17,7 @@ class CrossHttp
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*'); //允许所有资源跨域
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, Authorization, application/json , X-Auth-Token');//允许通过的响应报头
+        $response->header('Access-Control-Allow-Headers', '*');//允许通过的响应报头
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');//允许的请求方法
         $response->header('Access-Control-Expose-Headers', 'Authorization');//允许axios获取响应头中的Authorization
         $response->header('Allow', 'GET, POST, PATCH, PUT, OPTIONS, delete');//允许的请求方法
